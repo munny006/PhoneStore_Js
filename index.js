98df07a846a960ef97dccf54c14d9205c48e54b8
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-27 00:29:59 +0600

    card created

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const loadPhones = async() =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayPhones(data.data);
 }
 const displayPhones = phones =>{
-   const phoneContainer = document.getElementById('phones-container')
-   phones.foreach(phone =>{
-
+   const phonesContainer = document.getElementById('phones-container');
+   phones.forEach(phone =>{
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('col');
+    phoneDiv.innerHTML= `
+     <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+    `;
+    phonesContainer.appendChild(phoneDiv);
    })
 }
 loadPhones();
