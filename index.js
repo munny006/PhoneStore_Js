22cc58949dbdc846927b851669b09fc84c83781e
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-27 19:31:15 +0600

    design updated

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const displayPhones = phones =>{
     const phoneDiv = document.createElement('div');
     phoneDiv.classList.add('col');
     phoneDiv.innerHTML= `
-     <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+     <div class="card p-4">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title">${phone.phone_name}</h5>
                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                   </div>
